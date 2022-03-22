d78e1c2df742dce9688d7bea26a104328f1cda3d
Gayatri Patel <Gayatri.Patel@chas.group>
2022-03-22 01:12:02 +0000

    Add project files.

---
+++ WebScrapper.Data/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebScrapper.Data.Models;
+
+namespace WebScrapper.Data.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<SearchResults> SearchResults { get; set; }
+
+        public virtual DbSet<SearchEngines> SearchEngines { get; set; }
+    }
+}
